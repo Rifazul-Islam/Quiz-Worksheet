5cae4acc4993f8493952522b33b14ee60c60713b
Rifazul <rifazul50@gmail.com>
2022-10-10 23:36:27 +0600

    add error page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Home from './components/Home/Home';
 import Main from './components/Layout/Main';
+import Rechart from './components/Rechart/Rechart';
 
 function App() {
   const router = createBrowserRouter([
 
-    { path: '/', element: <Main></Main> }
+    {
+      path: '/', element: <Main></Main>, errorElement: <ErrorPage />, children: [
+
+        { path: '/', element: <Home></Home> },
+        { path: '/home', element: <Home></Home> },
+        { path: '/rechart', element: <Rechart></Rechart> },
+        { path: '/blog', element: <Blog></Blog> }
+      ]
+    }
 
   ])
   return (
